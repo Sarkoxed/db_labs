f2e0c7e234d721e16ee48748ce49c2d7605c1d29
Sarkoxed <sarkoxed2013@yandex.ru>
2023-05-24 22:50:25 +0300

    imporved sql init

--- 1_lab/gnomic_cart_2.sql
+++ 1_lab/gnomic_cart_2.sql
@@ -1,83 +1,56 @@
-CREATE TABLE `games` (
-	`id` int NOT NULL,
-	`title` TEXT NOT NULL,
-	`release_date` int NOT NULL,
-	`genre_id` int NOT NULL,
-	`platform_id` int NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE games (
+	id int,
+	title text,
+	release_date int,
+	genre_id int,
+	platform_id int
 );
 
-CREATE TABLE `players` (
-	`id` int NOT NULL,
-	`username` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE players (
+	id int,
+	username text
 );
 
-CREATE TABLE `developers` (
-	`id` int NOT NULL,
-	`name` TEXT NOT NULL,
-	`office_room_id` int NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE developers (
+	id int,
+	name text,
+	office_room_id int
 );
 
-CREATE TABLE `officerooms` (
-	`id` int NOT NULL,
-	`room_name` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE officerooms (
+	id int,
+	room_name text
 );
 
-CREATE TABLE `sales` (
-	`id` int NOT NULL,
-	`game_id` int NOT NULL,
-	`player_id` int NOT NULL,
-	`sale_date` int NOT NULL,
-	`price` int NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE sales (
+	id int,
+	game_id int,
+	player_id int,
+	sale_date int,
+	price int
 );
 
-CREATE TABLE `genres` (
-	`id` int NOT NULL,
-	`name` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE genres (
+	id int,
+	name text
 );
 
-CREATE TABLE `platforms` (
-	`id` int NOT NULL,
-	`name` TEXT NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE platforms (
+	id int,
+	name text
 );
 
-CREATE TABLE `ratings` (
-	`id` int NOT NULL,
-	`game_id` int NOT NULL,
-	`player_id` int NOT NULL,
-	`rating` int NOT NULL,
-	PRIMARY KEY (`id`)
+CREATE TABLE ratings (
+	id int,
+	game_id int,
+	player_id int,
+	rating int
 );
 
-CREATE TABLE `developed_by` (
-	`developer_id` int NOT NULL,
-	`game_id` int NOT NULL,
-	PRIMARY KEY (`developer_id`,`game_id`)
+CREATE TABLE developed_by (
+	developer_id int,
+	game_id int
 );
-
-ALTER TABLE `games` ADD CONSTRAINT `games_fk0` FOREIGN KEY (`genre_id`) REFERENCES `genres`(`id`);
-
-ALTER TABLE `games` ADD CONSTRAINT `games_fk1` FOREIGN KEY (`platform_id`) REFERENCES `platforms`(`id`);
-
-ALTER TABLE `developers` ADD CONSTRAINT `developers_fk0` FOREIGN KEY (`office_room_id`) REFERENCES `officerooms`(`id`);
-
-ALTER TABLE `sales` ADD CONSTRAINT `sales_fk0` FOREIGN KEY (`game_id`) REFERENCES `games`(`id`);
-
-ALTER TABLE `sales` ADD CONSTRAINT `sales_fk1` FOREIGN KEY (`player_id`) REFERENCES `players`(`id`);
-
-ALTER TABLE `ratings` ADD CONSTRAINT `ratings_fk0` FOREIGN KEY (`game_id`) REFERENCES `games`(`id`);
-
-ALTER TABLE `ratings` ADD CONSTRAINT `ratings_fk1` FOREIGN KEY (`player_id`) REFERENCES `players`(`id`);
-
-ALTER TABLE `developed_by` ADD CONSTRAINT `developed_by_fk0` FOREIGN KEY (`developer_id`) REFERENCES `developers`(`id`);
-
-ALTER TABLE `developed_by` ADD CONSTRAINT `developed_by_fk1` FOREIGN KEY (`game_id`) REFERENCES `games`(`id`);
 
 
 
---
+++ 2_lab/gnomic_cart_2.sql
@@ -0,0 +1,1 @@
+../1_lab/gnomic_cart_2.sql
